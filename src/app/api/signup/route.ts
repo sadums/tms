cf5911ebf79eaaba034ca91b9d822c4d44d2a032
Sam A <samuel.adams@ifit.com>
2025-02-05 12:22:41 -0700

    adjusted signup endpoint to act in accordance with login

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -8,10 +8,10 @@ export async function POST(request: Request) {
   try {
     const { email, password } = await request.json()
     if(!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email))){
-      return new Response('Inavlid email', { status: 400 });
+      return new Response('Invalid email', { status: 400 });
     }
     if(password.length < 8){
-      return new Response('Inavlid password', { status: 400 });
+      return new Response('Invalid password', { status: 400 });
     }
 
     await connect();
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,79 @@
-export default function SignUp(){
-    return(
-        <div>Sign up page</div>
-    )
-}
+// pages/login.js
+'use client'
+import { useState } from 'react';
+
+const SignupPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignup = async (e: any) => {
+    if(password != confirmPassword){
+        setError('Passwords do not match');
+        return
+    }
+    if(password.length < 8){
+        setError('Password must be at least 8 characters')
+        return
+    }
+    e.preventDefault();
+    
+    const res = await fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+    } else {
+      const errorData = await res.json();
+      setError(errorData.message || 'Something went wrong');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Confirm Password</label>
+          <input
+            id="confirmPassword"
+            type="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
